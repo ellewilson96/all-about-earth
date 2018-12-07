5958114e522c812e04dfb78dd70f54372592b28e
ellewilson96 <eleanorwilsonn@gmail.com>
2018-12-06 22:36:23 -0500

    Rename ServiceWorker file and added packages

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+//reducer imports go here
+import thunk from "redux-thunk";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+registerServiceWorker();
